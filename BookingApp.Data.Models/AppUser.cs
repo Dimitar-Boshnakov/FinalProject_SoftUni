8cadd4a76778d536f44c9f4a7a55c42b7405c465
Dimitar-Boshnakov <mitkoboshnakov2@gmail.com>
2024-12-05 12:48:57 +0200

    Creating project

---
+++ BookingApp.Data.Models/AppUser.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BookingApp.Data.Models
+{
+    using static BookingApp.Common.EntityValidations;
+
+    public class AppUser : IdentityUser<Guid>
+    {
+        public AppUser() 
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [Required]
+        [MaxLength(AppUserValidation.FirstNameMaxLength)]
+        [Comment("User First Name")]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(AppUserValidation.LastNameMaxLength)]
+        [Comment("User Last Name")]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        [Comment("User Email")]
+        public string UserEmail {  get; set; } = null!;
+
+        public virtual ICollection<Property> Properties { get; set; } = new HashSet<Property>();
+    }
+}
---
+++ BookingApp.Data.Models/PropertyAmenity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookingApp.Data.Models
+{
+    [PrimaryKey(nameof(PropertyId), nameof(AmenityId))]
+
+    public class PropertyAmenity
+    {
+        [Required]
+        public Guid PropertyId { get; set; }
+
+        [ForeignKey(nameof(PropertyId))]
+        public virtual Property Property { get; set; } = null!;
+
+        [Required]
+        public Guid AmenityId { get; set; }
+
+        [ForeignKey(nameof(AmenityId))]
+        public virtual Amenity Amenity { get; set; } = null!;
+
+    }
+}
