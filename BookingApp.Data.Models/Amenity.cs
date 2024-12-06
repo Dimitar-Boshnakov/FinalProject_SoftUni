64dc40b1f0df020415859b2c2a139f2e297b57e8
Dimitar-Boshnakov <mitkoboshnakov2@gmail.com>
2024-12-06 19:11:40 +0200

    Delete Amenity.cs

--- BookingApp.Data.Models/Amenity.cs
+++ BookingApp.Data.Models/Amenity.cs
@@ -1,22 +1,4 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
-namespace BookingApp.Data.Models
-{
-    using static BookingApp.Common.EntityValidations;
-    public class Amenity
-    {
-        [Key]
-        public Guid Id { get; set; } = Guid.NewGuid();
-
-        [Required]
-        [MaxLength(AmenityValidation.AmenityNameMaxLength)]
-        public string AmenityName { get; set; } = null!;
-
-        public virtual ICollection<PropertyAmenity> PropertiesAmenities { get; set; } = new HashSet<PropertyAmenity>();
-    }
-}
